6c0cc6545c99f6e151fc0974bbda0e4a41c400f9
harmo <dtharmo@gmail.com>
2016-02-06 12:18:58 +0100

    Add us static page

--- indochine/urls.py
+++ indochine/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from oscar.app import application
 from oscar.core.loading import get_class
 
@@ -13,6 +14,9 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'la-carte/', include('indochine.apps.catalogue.urls', namespace='card')),
+    url(r'mon-compte/', TemplateView.as_view(template_name='static-pages/account.html'), name='account'),
+    url(r'qui-sommes-nous/', TemplateView.as_view(template_name='static-pages/us.html'), name='us'),
+    url(r'groupes-et-pros/', TemplateView.as_view(template_name='static-pages/groups-and-pros.html'), name='pros'),
     url(r'', include(application.urls)),
 
 ]
